3c762f33998b41303668309ae99e24ca8523ad09
Matthew King <mking38@byu.edu>
2026-02-25 16:33:30 -0700

    impl(p3) added data access and service classes; 0/22 tests passed

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,8 @@
+package service;
+
+public class UserService {
+//    ** Code from project specs **
+//    public RegisterResult register(RegisterRequest registerRequest) {}
+//    public LoginResult login(LoginRequest loginRequest) {}
+//    public void logout(LogoutRequest logoutRequest) {}
+}
