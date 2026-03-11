b151edb9205e3e36003de48e6266dadf3ca4a7de
Matthew King <mking38@byu.edu>
2026-03-11 01:39:01 -0600

    impl(p4) implemented createGame

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,10 +1,14 @@
 package dataaccess;
 
 import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -26,8 +30,32 @@ public class SQLGameDAO implements GameDAO {
         DatabaseManager.configureDatabase(createStatement);
     }
 
-    public GameData createGame(String gameName) {
-        return new GameData(1, null, null, null, new ChessGame());
+    public GameData createGame(String gameName) throws DataAccessException {
+
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.setCatalog("chess");
+            var statement = "INSERT INTO games (gameName, game) VALUES (?, ?)";
+            ChessGame game = new ChessGame();
+            try (var preparedStatement = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, gameName);
+                preparedStatement.setString(2, new Gson().toJson(game));
+                preparedStatement.executeUpdate();
+
+                int id;
+                ResultSet rs = preparedStatement.getGeneratedKeys();
+                if (rs.next()) {
+                    id = rs.getInt(1);
+                } else {
+                    id = 0;
+                }
+
+                return new GameData(
+                        id, null, null, gameName, game);
+            }
+
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to create game");
+        }
     }
 
     public GameData updateGame(int gameID, String userName, String playerColor) {
