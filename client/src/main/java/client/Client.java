ee5a793725460ed6472746056a64940714453977
Matthew King <mking38@byu.edu>
2026-03-24 09:52:09 -0600

    impl(p5) started preloginUI

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,12 +1,20 @@
 package client;
 
+import ui.*;
+import static ui.EscapeSequences.*;
+
 public class Client {
-    "♕ 240 Chess Client:"
+    private final ServerFacade server;
 
     public Client(String serverUrl) throws Exception {
         server = new ServerFacade(serverUrl);
     }
 
     public void run() {
+        PreloginUI.run();
+    }
+
+    public static void printPrompt() {
+        System.out.print("\n" +  ">>> " + SET_TEXT_COLOR_GREEN);
     }
 }
--- client/src/main/java/ui/PreloginUI.java
+++ client/src/main/java/ui/PreloginUI.java
@@ -1,4 +1,51 @@
 package ui;
 
+import client.Client;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class PreloginUI {
+
+    public static void run() {
+        System.out.println("♕ Welcome to 240 Chess! Type help to get started");
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            Client.printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    public static String eval(String input) {
+//        try {
+//            String[] tokens = input.toLowerCase().split(" ");
+//            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+//            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+//            return switch (cmd) {
+//                case "signin" -> signIn(params);
+//                case "rescue" -> rescuePet(params);
+//                case "list" -> listPets();
+//                case "signout" -> signOut();
+//                case "adopt" -> adoptPet(params);
+//                case "adoptall" -> adoptAllPets();
+//                case "quit" -> "quit";
+//                default -> help();
+//            };
+//        } catch (ResponseException ex) {
+//            return ex.getMessage();
+//        }
+        return "Hello World";
+    }
 }
