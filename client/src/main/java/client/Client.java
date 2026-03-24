f2b3d5e887caaa5761ba5f3b7adfa2599f75e134
Matthew King <mking38@byu.edu>
2026-03-24 09:20:40 -0600

    impl(p5) implemented ClientMain

---
+++ client/src/main/java/client/Client.java
@@ -0,0 +1,12 @@
+package client;
+
+public class Client {
+    "♕ 240 Chess Client:"
+
+    public Client(String serverUrl) throws Exception {
+        server = new ServerFacade(serverUrl);
+    }
+
+    public void run() {
+    }
+}
