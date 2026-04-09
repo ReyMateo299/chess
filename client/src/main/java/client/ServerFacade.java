fc6e0ad4a61144a71245aa8de19c01e077e33cd9
Matthew King <mking38@byu.edu>
2026-04-09 17:43:19 -0600

    new changes

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -75,7 +75,6 @@ public class ServerFacade {
         var path = "/db";
         var httpRequest = buildRequest("DELETE", path, null);
         sendRequest(httpRequest);
-//        return handleResponse(response, RegisterResult.class);
     }
 
     private HttpRequest buildRequest(String method, String path, Object body) {
