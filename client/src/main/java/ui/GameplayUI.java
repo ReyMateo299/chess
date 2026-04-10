a1b4382f748c1276c2c95d3d300a986358c51cac
Matthew King <mking38@byu.edu>
2026-04-09 18:47:24 -0600

    impl(p6) worked on makeMove

--- client/src/main/java/ui/GameplayUI.java
+++ client/src/main/java/ui/GameplayUI.java
@@ -15,8 +15,7 @@ import requests.GetGamesRequest;
 import results.GetGamesResult;
 import websocket.commands.MakeMoveCommand;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 import static ui.EscapeSequences.*;
 
@@ -66,15 +65,9 @@ public class GameplayUI {
     }
 
     private UIResult redrawBoard() throws ResponseException {
-        GetGamesResult result = server.getGames(new GetGamesRequest(authToken));
-        ChessGame game = null;
-        for (GameData gameData : result.games()) {
-            if (gameData.gameID() == gameID) {
-                game = gameData.game();
-            }
-        }
+        ChessGame game = getChessGame();
         if (game == null) {
-            return new UIResult("No Game found", State.GAMEPLAY, authToken, null);
+            throw new ResponseException("Game not found");
         }
         String message = ChessBoardPrinter.printChessBoard(playerColor, game.getBoard());
         return new UIResult(message, State.GAMEPLAY, authToken, null);
@@ -86,12 +79,69 @@ public class GameplayUI {
     }
 
     private UIResult makeMove(String... params) throws ResponseException {
-        String message = "make move\n";
-        ws.sendCommand(new MakeMoveCommand(
-                authToken, gameID, new ChessMove(
-                        new ChessPosition(1, 1), new ChessPosition(1, 1), ChessPiece.PieceType.QUEEN)
-        ));
-        return new UIResult(message, State.GAMEPLAY, authToken, null);
+        if (params.length >= 1 && params[0].length() == 4) {
+            String moveInput = params[0];
+            Map<Character, Integer> cols = new HashMap<>(Map.of(
+                    'a', 1,
+                    'b', 2,
+                    'c', 3,
+                    'd', 4,
+                    'e', 5,
+                    'f', 6,
+                    'g', 7,
+                    'h', 8));
+            Character startColChar = moveInput.charAt(0);
+            Character endColChar = moveInput.charAt(2);
+            Integer startCol = cols.get(startColChar);
+            Integer endCol = cols.get(endColChar);
+            int startRow = Character.getNumericValue(moveInput.charAt(1));
+            int endRow = Character.getNumericValue(moveInput.charAt(1));
+
+            ChessGame game = getChessGame();
+            if (game == null) {
+                throw new ResponseException("No Game found");
+            }
+
+            var startPosition = new ChessPosition(startRow, startCol);
+            var endPosition = new ChessPosition(endRow, endCol);
+            var startPiece = game.getBoard().getPiece(startPosition);
+
+            if (startPiece == null || startPiece.getTeamColor() != playerColor) {
+                throw new ResponseException("Invalid move");
+            }
+
+            ChessPiece.PieceType promotionPiece = null;
+
+            if ((endRow == 1 && playerColor == TeamColor.BLACK) ||
+                    (endRow == 8 && playerColor == TeamColor.WHITE)) {
+                Set<String> validPromotions = new HashSet<>(Set.of("N", "B", "R", "Q"));
+
+                boolean validResponse = false;
+                while (!validResponse) {
+                    Scanner secondScanner = new Scanner(System.in);
+                    System.out.print("\n" + RESET_TEXT_COLOR + "Options: N, B, R, Q");
+                    System.out.print("\n" + "[IN_GAME] Enter Promotion piece type >>> " + SET_TEXT_COLOR_GREEN);
+                    String line = secondScanner.nextLine();
+                    String[] tokens = line.toLowerCase().split(" ");
+                    String pieceString = tokens[0];
+                    if (tokens.length == 1 && validPromotions.contains(pieceString)) {
+                        validResponse = true;
+                        switch (pieceString) {
+                            case "N" -> promotionPiece = ChessPiece.PieceType.KNIGHT;
+                            case "B" -> promotionPiece = ChessPiece.PieceType.BISHOP;
+                            case "R" -> promotionPiece = ChessPiece.PieceType.ROOK;
+                            case "Q" -> promotionPiece = ChessPiece.PieceType.QUEEN;
+                        }
+                    }
+                }
+            }
+
+            ws.sendCommand(new MakeMoveCommand(
+                    authToken, gameID, new ChessMove(startPosition, endPosition, promotionPiece)
+            ));
+            return new UIResult("Move was made", State.GAMEPLAY, authToken, null);
+        }
+        throw new ResponseException("Expected form: make <StartPosition><EndPosition>    Example: make e2e4");
     }
 
     private UIResult resign() throws ResponseException {
@@ -102,6 +152,17 @@ public class GameplayUI {
     private UIResult highlightMoves() throws ResponseException {
         String message = "highlight\n";
         return new UIResult(message, State.GAMEPLAY, authToken, null);
+    }
+
+    private ChessGame getChessGame() throws ResponseException {
+        GetGamesResult result = server.getGames(new GetGamesRequest(authToken));
+        ChessGame game = null;
+        for (GameData gameData : result.games()) {
+            if (gameData.gameID() == gameID) {
+                game = gameData.game();
+            }
+        }
+        return game;
     }
 
     private UIResult help() {
